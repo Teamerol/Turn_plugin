8b3fc493ae952601f38f48cc943269e88c399f47
Teamerol <fadeew.alex@mail.ru>
2022-04-08 06:01:55 +0300

    Added logging for processing scripts.

--- algorithms/RotationAlgorithm.py
+++ algorithms/RotationAlgorithm.py
@@ -1,6 +1,9 @@
 from qgis import processing
+from qgis.core import QgsProcessingException
+import logging
 from ..MainWindowRotationProvider import MainWindowRotationProvider
 
+basicConfig = logging.basicConfig(filename = "Turn_plugin.log", filemode = "w", format = '%(asctime)s - %(message)s', datefmt = '%d-%b-%y %H:%M:%S')
 
 class RotationAlgorithm:
     """Implements logic for rotating vector layer."""
@@ -11,15 +14,22 @@ class RotationAlgorithm:
     def findCentroid(self):
         first_object_id = 1
 
-        centroids_alg = processing.run("native:centroids",
-                                       {"INPUT": self.parameters.processingLayer,
-                                        "OUTPUT": "memory:"})
-        layer = centroids_alg["OUTPUT"]
-        point = layer.getGeometry(fid = first_object_id).asPoint()
+        try:
+            centroids_alg = processing.run("native:centroids",
+                                        {"INPUT": self.parameters.processingLayer,
+                                            "OUTPUT": "memory:"})
+            layer = centroids_alg["OUTPUT"]
+            point = layer.getGeometry(fid = first_object_id).asPoint()
+        except QgsProcessingException:
+            logging.exception("QgsProcessingException while finding anchor centroid.")
+
         return point
 
     def rotate(self):
-        return processing.runAndLoadResults("native:rotatefeatures", {"INPUT": self.parameters.processingLayer,
-                                                        "ANGLE": self.parameters.rotationAngle,
-                                                        "ANCHOR": self.findCentroid(),
-                                                        "OUTPUT": self.parameters.outputName})
+        try:
+            return processing.runAndLoadResults("native:rotatefeatures", {"INPUT": self.parameters.processingLayer,
+                                                            "ANGLE": self.parameters.rotationAngle,
+                                                            "ANCHOR": self.findCentroid(),
+                                                            "OUTPUT": self.parameters.outputName})
+        except QgsProcessingException:
+            logging.exception("QgsProcessingException while rotating features.")
